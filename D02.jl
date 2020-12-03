760e7b207d9df7336c4d860b0b247bc79a06b2d9
csLinhart <36882208+csLinhart@users.noreply.github.com>
2020-12-03 06:24:14 +0100

    Update D02.jl

--- D02.jl
+++ D02.jl
@@ -3,19 +3,14 @@ input = readdlm("D02.txt", '\n', String)
 input = split.(input, " ")
 
 function D02(input)
-    count1 = 0
-    count2 = 0
+    count1 = count2 = 0
     for i = 1:length(input)
         pwd = input[i][3]
         cm = countmap([c for c in pwd])
         r = parse.(Int, split(input[i][1], "-"))
         letter = input[i][2][1]
-        if occursin(letter, pwd) && r[1] <= cm[letter]  <= r[2]
-            count1 += 1
-        end
-        if xor(pwd[r[1]] == letter, pwd[r[2]] == letter)
-            count2 += 1
-        end
+        count1 += occursin(letter, pwd) && r[1] <= cm[letter]  <= r[2] ? 1 : 0
+        count2 += xor(pwd[r[1]] == letter, pwd[r[2]] == letter) ? 1 : 0
     end
     (count1, count2)
 end
