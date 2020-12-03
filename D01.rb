771e59dcee747cea2587ce934e46b6d1c3d82a3a
csLinhart <36882208+csLinhart@users.noreply.github.com>
2020-12-03 12:48:28 +0100

    Update D01.rb

--- D01.rb
+++ D01.rb
@@ -1,4 +1,4 @@
-input = File.read("C:\\Repos\\AoC\\AoC20\\D01.txt").split().map(&:to_i)
+input = File.read("D01.txt").split().map(&:to_i)
 
 def D01(input, n)
     input.combination(n).select { |x| x.sum == 2020}[0].reduce(:*)
