4cc027478e580cbc1570cc6ab103988a09de38b3
csLinhart <36882208+csLinhart@users.noreply.github.com>
2020-12-05 10:15:48 +0100

    Update D05.rb

--- D05.rb
+++ D05.rb
@@ -2,7 +2,7 @@ input = File.read("D05.txt").split().map(&:to_s)
 
 def D05(input)
     seats = input.map {|x| x.gsub("F", "0").gsub("B", "1").gsub("L", "0").gsub("R", "1").to_i(2)}
-    id = (1..seats.max).select.with_index { |_, i|  seats.include?(i - 1) && seats.include?(i + 1) && !seats.include?(i)}
+    id = (1..seats.max).select{ |i| seats.include?(i - 1) && seats.include?(i + 1) && !seats.include?(i)}
     [seats.max, id]
 end
 
