5abbf296ef832542094dffcdf993c39798a72fc3
csLinhart <36882208+csLinhart@users.noreply.github.com>
2020-12-12 08:23:25 +0100

    Update D12.jl

--- D12.jl
+++ D12.jl
@@ -4,30 +4,19 @@ input = readdlm("D12.txt", '\n', String)
 function D12(input)
     dir = [0,1]
     pos1 = pos2 = [0,0]
+    nsew = Dict('N' => [1,0],'S' => [-1,0],'E' => [0,1],'W' => [0,-1])
+    rot(n) = Dict('R' => [cosd(n) -sind(n); sind(n) cosd(n)],'L' => [cosd(n) sind(n); -sind(n) cosd(n)])
     wp = [1, 10]
     for line in input
         a = line[1]
         n = parse(Int, line[2:end])
-        if a == 'N'
-            pos1 += [1,0] * n
-            wp += [1,0] * n
-        elseif a == 'S'
-            pos1 += [-1,0] * n
-            wp += [-1,0] * n
-        elseif a == 'E'
-            pos1 += [0,1] * n
-            wp += [0,1] * n
-        elseif a == 'W'
-            pos1 += [0,-1] * n
-            wp += [0,-1] * n
-        elseif a == 'L'
-            rl = [cosd(n) sind(n); -sind(n) cosd(n)]
-            dir = rl * dir
-            wp = rl * wp
-        elseif a == 'R'
-            rr = [cosd(n) -sind(n); sind(n) cosd(n)]
-            dir = rr * dir
-            wp = rr * wp
+        if a in keys(nsew)
+            pos1 += nsew[a] * n
+            wp += nsew[a] * n
+        elseif a in keys(rot(n))
+            r = rot(n)[a]
+            dir = r * dir
+            wp = r * wp
         elseif a == 'F'
             pos1 += dir * n
             pos2 += wp * n
