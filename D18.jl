76fb9fe3540ed0cefc859bf7dcea9b537fbfc4cb
csLinhart <36882208+csLinhart@users.noreply.github.com>
2020-12-18 10:07:08 +0100

    Update D18.jl

--- D18.jl
+++ D18.jl
@@ -1,18 +1,19 @@
 using DelimitedFiles
 input = readdlm("D18.txt", '\n', String)
 
+*₁(a, b) = a + b
++₁(a, b) = a * b
+
 function D18(input)
-    *₃(a, b) = a + b
-    +₄(a, b) = a * b
     sum1 = sum2 =0
     for line in input
         sl1 = split(line, "")
-        sl1[sl1 .== "+"] .= "*₃"
+        sl1[sl1 .== "+"] .= "*₁"
         sum1 += eval(Meta.parse(join(sl1)))
         
         sl2 = split(line, "")
-        sl2[sl2 .== "*"] .= "+₄"
-        sl2[sl2 .== "+"] .= "*₃"
+        sl2[sl2 .== "*"] .= "+₁"
+        sl2[sl2 .== "+"] .= "*₁"
         sum2 += eval(Meta.parse(join(sl2)))
     end
     (sum1, sum2)
